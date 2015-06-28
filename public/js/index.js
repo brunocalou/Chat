1af4fd43d5c9441410a08aa338180eadbdd7ea74
brunocalou <brunocaloualves@gmail.com>
2015-06-28 10:32:48 -0300

    Added user has entered / left message. Added js and css files

--- public/js/index.js
+++ public/js/index.js
@@ -1,94 +1,82 @@
-// angular.module('chatApp', [])
-//   .controller('ChatController', function() {
-//
-//   });
-
-
 // function() {
-  var last_user = '';
-  var user = '';
-  var socket = io();
-  var users = [];
-  
-  var send = function() {
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  };
+var last_user = '';
+var user = '';
+var socket = io();
+var users = [];
 
-//  var authenticate = function(user) {
-//    socket.emit('authentication', user);
-//  }
+var send = function() {
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
+};
 
-  socket.on('connect', function() {
-//    authenticate(user);
-  });
+socket.on('authentication', function(username) {
+  user = username;
+  console.log("My username is " + user);
+});
 
-  socket.on('authentication', function(username) {
-    user = username;
-    console.log("My username is " + user);
-  });
+$('form').submit(function() {
+  return send();
+});
 
-  $('form').submit(function() {
-    return send();
-  });
+socket.on('chat message', function(msg) {
+  var p = $('<p></p>');
+  p.append(msg['msg']);
 
-  socket.on('chat message', function(msg) {
-    var p = $('<p></p>');
-    p.append(msg['msg']);
-    
-    //If the last user to send a message is the same
-    if(last_user == msg['user']) {
-      $('#messages li:last-child div').append(p);
-      
+  //If the last user to send a message is the same
+  if (last_user == msg['user']) {
+    $('#messages li:last-child div').append(p);
+
+  } else {
+    last_user = msg['user'];
+    var li = $('<li></li>');
+    var div = $('<div></div>');
+
+    //If who sent the message is receiving it
+    if (msg['user'] == user) {
+      li.addClass('own-message');
     } else {
-      last_user = msg['user'];
-      var li = $('<li></li>');
-      var div = $('<div></div>');
-
-      //If who sent the message is receiving it
-      if(msg['user'] == user) {
-        li.addClass('own-message');
-      } else {
-        var user_p = $('<p></p>');
-        user_p.append(last_user);
-        user_p.addClass('user');
-        div.append(user_p);
-      }
-
-      div.append(p);
-      li.append(div);
-      $('#messages').append(li);
+      var user_p = $('<p></p>');
+      user_p.append(last_user);
+      user_p.addClass('user');
+      div.append(user_p);
     }
-    
-    //Scroll the chat
-    $("#chat-view").prop({ scrollTop: $("#chat-view").prop("scrollHeight")});
-    
-  });
 
-  socket.on('error', function(msg) {
-    console.log(msg);
-  })
-  
-  socket.on('user-update', function(users_list) {
-    users = users_list;
-    console.log(users);
-    updateUserList();
-  });
-
-  var updateUserList = function() {
-    var DOM_users_list = $('#users-section ul');
-    DOM_users_list.empty();
-    for(var i = 0; i < users.length; i++) {
-      console.log(users[i]);
-      var li = $('<li></li>');
-      if(users[i] == user) {
-        li.addClass('own-user');
-      }
-      li.append(users[i]);
-      DOM_users_list.append(li);
-      
-    }
+    div.append(p);
+    li.append(div);
+    $('#messages').append(li);
   }
+
+  //Scroll the chat
+  $("#chat-view").prop({
+    scrollTop: $("#chat-view").prop("scrollHeight")
+  });
+
+});
+
+socket.on('error', function(msg) {
+  console.log(msg);
+})
+
+socket.on('user-update', function(users_list) {
+  users = users_list;
+  console.log(users);
+  updateUserList();
+});
+
+var updateUserList = function() {
+  var DOM_users_list = $('#users-section ul');
+  DOM_users_list.empty();
+  for (var i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    var li = $('<li></li>');
+    if (users[i] == user) {
+      li.addClass('own-user');
+    }
+    li.append(users[i]);
+    DOM_users_list.append(li);
+
+  }
+}
 
 // }();
--- server.js
+++ server.js
@@ -18,18 +18,18 @@ var express = require('express'),
   username_generator = 0; //Holds the username when the available usernames are empty
 
 var sendUsersList = function() {
-    var users_update = [];
-    for(user_id in users) {
-      users_update.push(users[user_id]);
-    }
-    console.log("sending " + users_update);
-    io.emit('user-update', users_update);
-  };
+  var users_update = [];
+  for (user_id in users) {
+    users_update.push(users[user_id]);
+  }
+  console.log("sending " + users_update);
+  io.emit('user-update', users_update);
+};
 
 var generateUsername = function(socket) {
   //Get a username
   var username;
-  if(available_usernames.length != 0) {
+  if (available_usernames.length != 0) {
     var index = Math.floor(Math.random() * available_usernames.length);
     username = available_usernames[index];
     //Remove the username from the list
@@ -49,34 +49,42 @@ var generateUsername = function(socket) {
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket) {
-  
+
 
   generateUsername(socket);
   sendUsersList(socket);
-  
+  io.emit('chat message', {'usr': '', 'msg': users[socket.id] + ' has entered'});
 
-  socket.on('disconnect', function(){
-    if(users.hasOwnProperty(socket.id))
-    {
-      if(isNaN(users[socket.id])) {
+  socket.on('disconnect', function() {
+    var message = {
+      'user': '',
+      'msg': ''
+    };
+
+    if (users.hasOwnProperty(socket.id)) {
+      if (isNaN(users[socket.id])) {
         available_usernames.push(users[socket.id]);
       } else {
         username_generator -= 1;
       }
-      
+      message['msg'] = users[socket.id] + ' has left';
       delete users[socket.id];
     }
     sendUsersList();
-    
+
+    io.emit('chat message', message);
     console.log('user disconnected');
   });
-  
+
   socket.on('chat message', function(msg) {
-    if(users[socket.id] != undefined) {
+    if (users[socket.id] != undefined) {
       console.log(users);
       var username = users[socket.id];
       console.log(username);
-      var message = {'user': username, 'msg':msg};
+      var message = {
+        'user': username,
+        'msg': msg
+      };
       console.log(message);
       io.emit('chat message', message);
     }
